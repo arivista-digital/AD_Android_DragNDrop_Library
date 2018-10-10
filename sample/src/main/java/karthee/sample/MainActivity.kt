f1b8ea9e4a9651867b5cbc33ddf7a9ca8fe297df
rajchristhu <rajchristhu.antony@arivistadigital.com>
2018-10-10 13:57:37 +0530

    sample

--- sample/src/main/java/karthee/sample/MainActivity.kt
+++ sample/src/main/java/karthee/sample/MainActivity.kt
@@ -14,14 +14,12 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.hide()
         setContentView(R.layout.activity_main)
         val customView = findViewById<CustomView>(R.id.customView)
-        data.add(Optionmodel("14th Jan", 900, 300, 320, 80))
-        data.add(Optionmodel("15th Aug", 900, 100, 320, 80))
-        data.add(Optionmodel("25th Dec", 900, 500, 320, 80))
-        data.add(Optionmodel("12 May", 900, 1100, 320, 80))
-        data.add(Optionmodel("14th Feb", 900, 700, 320, 80))
-        data.add(Optionmodel("4 Aug", 900, 900, 320, 80))
-        customView.setInput(data)
-        customView.setImage(R.drawable.demoimage)
+        data.add(Optionmodel("Freedom to eat as I wish", 136, 72, 380, 35))
+        data.add(Optionmodel("15th Aug", 136, 108, 380, 35))
+        data.add(Optionmodel("25th Dec", 136, 144, 380, 35))
+        data.add(Optionmodel("12 May", 136, 181, 380, 35))
+        data.add(Optionmodel("14th Feb", 136, 218, 380, 35))
+        customView.setInput(data,R.drawable.p7)
     }
 }
 
